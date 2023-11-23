6e2a3aaf3454afc61b179a7972f6f1c3db277ddb
MechaChen <tumlivein@gmail.com>
2023-11-23 21:25:30 +0800

    feat: add left and right

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 import LoginPage from './pages/login';
 import Home from './pages/home';
 
+import Left from './components/Left';
+import Right from './components/Right';
+
 import './App.css'
 
 function App() {
   return (
-    <Home />
+    <>
+      <Left />
+      <Right />
+    </>
+    // <Home />
     // <LoginPage />
   )
 }
